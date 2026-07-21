ed424af4c90f9a7705694bde9ee850fdf194949b
decimaquintasp <decimaquintasp@gmail.com>
2026-07-21 17:47:44 -0300

    Configuração automática do setup

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-    PROXY_URL: 'https://polished-cherry-132b.migueldonizeti96.workers.dev',
-    CLIENT_ID: '998786899604-rver6agqntd9ueef1ecdosdqomu6lk3q.apps.googleusercontent.com',
-    SHEET_ID: '14SF9b4VV4KR7MR3DDcfpviX3fE764cA3ZPuMgk-ugY8',
-    SCOPES: 'https://www.googleapis.com/auth/spreadsheets profile email',
+    PROXY_URL: 'https://demolay-proxy.decimaquintasp.workers.dev',
+    SHEET_ID: '17xcpYAfxBav0U_a8dUYXU1kkYVbBqJElDypvzuKL3Cw',
+    SCOPES: 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.file profile email',
     SHEET_LINKS: 'Sheet1',
     SHEET_CONFIG: 'Sheet2',
     RANGE_LINKS: 'Sheet1!A:F',
     RANGE_CONFIG: 'Sheet2!A:B',
+    WIZARD_URL: 'https://demotree-wizard.vercel.app',
 };
