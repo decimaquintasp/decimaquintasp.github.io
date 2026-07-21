6e4c1ceca80c4d10672adc9f5f3eeb81a9cc6aa8
Miguel <silva.silva@unesp.br>
2026-07-21 01:47:43 -0300

    Troca GOOGLE_CLIENT_ID para o novo OAuth Client ID do decimaquinta

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     PROXY_URL: 'https://polished-cherry-132b.migueldonizeti96.workers.dev',
-    CLIENT_ID: '897683631001-ugml9ertq7bldbtmsugcejhitav6l4dp.apps.googleusercontent.com',
+    CLIENT_ID: '998786899604-rver6agqntd9ueef1ecdosdqomu6lk3q.apps.googleusercontent.com',
     SHEET_ID: '14SF9b4VV4KR7MR3DDcfpviX3fE764cA3ZPuMgk-ugY8',
     SCOPES: 'https://www.googleapis.com/auth/spreadsheets profile email',
     SHEET_LINKS: 'Sheet1',
